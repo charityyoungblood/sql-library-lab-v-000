381166ed0ac8a1d51cc62533211e39bcf718f421
charityyoungblood <youngblood.charity@gmail.com>
2018-01-12 13:19:06 -0800

    created characters table in schema.sql

--- lib/schema.sql
+++ lib/schema.sql
@@ -17,6 +17,16 @@ CREATE TABLE authors (
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY, 
+    title TEXT, 
     year INTEGER, 
     series_id INTEGER
 ); 
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    species TEXT, 
+    motto TEXT, 
+    series_id INTEGER, 
+    author_id INTEGER
+);
