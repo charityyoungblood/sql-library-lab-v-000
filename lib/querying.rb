73272f1c51f8b3f775a586f73d022d7191cfbfb7
charityyoungblood <youngblood.charity@gmail.com>
2018-01-17 14:02:33 -0800

    added order by clause to character name function in querying.rb

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,5 +21,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name FROM characters"
+  "SELECT characters.name, character_id FROM characters JOIN character_books WHERE character_books.id = character_id ORDER BY book_id DESC"
 end
