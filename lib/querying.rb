a8bf723c16ae91b9911c1486f7c337b7575168cc
charityyoungblood <youngblood.charity@gmail.com>
2018-01-17 13:34:34 -0800

    added query for series title in querying.rb

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,13 +12,14 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT title FROM series"
+  "SELECT authors.name, subgenres.name FROM authors JOIN subgenres WHERE subgenres.id = authors.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  # characters.species count = 'human' 
+  "SELECT title FROM series JOIN characters ON series.id = characters.series_id WHERE species = 'human' GROUP BY species HAVING count(*) > 3"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name FROM characters"
 end
